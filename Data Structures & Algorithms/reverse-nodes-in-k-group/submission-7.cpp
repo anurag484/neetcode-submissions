577c6bda54078b8e899815c7b91bb540a8302c87
Anurag Ruwali <114792427+anurag484@users.noreply.github.com>
2026-09-01 12:22:24 +0530

    Add: reverse-nodes-in-k-group - submission-7

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-7.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* getKthNode(ListNode* temp,int k){
+        k--;
+        while(k>0 && temp){
+            k--;
+            temp=temp->next;
+        }
+        return temp;
+    }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr){
+            ListNode* front=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=front;
+        }
+        return prev;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head || k==1){
+            return head;
+        }
+        ListNode* prevLast=NULL;
+        ListNode* newHead=NULL;
+        ListNode* temp=head;
+        while(temp){
+            ListNode* kthNode=getKthNode(temp,k);
+            if(kthNode==NULL){
+                if(prevLast){
+                    prevLast->next=temp;
+                }
+                break;
+            }
+            ListNode* nextNode=kthNode->next;
+            kthNode->next=NULL;
+
+            ListNode* reverseHead=reverse(temp);
+            if(newHead==NULL){
+                newHead=reverseHead;
+            }else{
+                prevLast->next=reverseHead;
+            }
+            prevLast=temp;
+            temp=nextNode;
+        }
+        return newHead;
+    }
+};
